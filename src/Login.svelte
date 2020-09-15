f679e72b0b2123dfcac5e3d78d1cfde6cb50433c
Praveen Kumar S <praveens.bec@gmail.com>
2020-09-15 11:18:42 +0530

    Improved login page

---
+++ src/Login.svelte
@@ -0,0 +1,35 @@
+<style>
+  .bg-teal {
+    background-color: #009688;
+  }
+</style>
+<main>
+  <div class="jumbotron jumbotron-fluid bg-teal">
+    <h1 class="display-4 text-white p-2">Reenigne</h1>
+  </div>
+  <div class="container">
+    <div class="row">
+      <form class="col-md-6 offset-md-3 col-lg-4 offset-lg-4 shadow p-3">
+        <div class="form-group">
+          <label class="h5 font-weight-normal" for="username">Username</label>
+          <input
+            class="form-control"
+            id="username"
+            type="text"
+            name="username"
+          />
+        </div>
+        <div class="form-group">
+          <label class="h5 font-weight-normal" for="password">Password</label>
+          <input
+            class="form-control"
+            id="password"
+            type="password"
+            name="password"
+          />
+        </div>
+        <input class="form-control btn-primary" type="submit" value="Login" />
+      </form>
+    </div>
+  </div>
+</main>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
-import App from "./App.svelte";
+//import App from "./App.svelte";
+import Login from "./Login.svelte";
 
-const app = new App({
+// const app = new App({
+//   target: document.body,
+//   props: {},
+// });
+
+const login = new Login({
   target: document.body,
   props: {},
 });
-
-export default app;
+export default login;
