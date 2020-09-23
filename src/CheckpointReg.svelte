e0bae797ab84e098b434f9728a8a59df4bfede5c
Praveen Kumar S <praveens.bec@gmail.com>
2020-09-23 23:56:05 +0530

    Added Checkpoint registration page

--- src/CheckpointReg.svelte
+++ src/CheckpointReg.svelte
@@ -1,21 +1,86 @@
 <script>
   export let SERVER;
+  import L from "leaflet";
+  import { onMount } from "svelte";
+  let map;
+  let latlng, name, id, pwd;
+  let alert_msg,
+    alert_class = "invisible";
+  let marker = new L.marker();
+  onMount(() => {
+    let options = {
+      center: [0, 0],
+      zoom: 2,
+    };
+    map = L.map("map", options);
+    L.tileLayer("https://tiles.wmflabs.org/hikebike/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors | Tiles: wmflabs',
+    }).addTo(map);
+    map.on("click", (e) => {
+      latlng = e.latlng;
+      marker.setLatLng(latlng);
+      marker.addTo(map);
+    });
+  });
+
+  async function submit() {
+    if (!name) {
+      alert_class = "alert-danger";
+      alert_msg = "Please enter Name";
+    } else if (!id) {
+      alert_class = "alert-danger";
+      alert_msg = "Please enter Id";
+    } else if (!pwd) {
+      alert_class = "alert-danger";
+      alert_msg = "Please enter Password";
+    } else if (!latlng) {
+      alert_class = "alert-danger";
+      alert_msg = "Please choose location on map";
+    } else {
+      try {
+        alert_class = "alert-warning";
+        alert_msg = "loading...";
+        const data = await fetch(SERVER + "/cp/create", {
+          method: "POST",
+          mode: "cors",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + localStorage.getItem("token"),
+          },
+          body: JSON.stringify({
+            name: name,
+            id: id,
+            lat: latlng.lat,
+            lon: latlng.lng,
+            authSecret: pwd,
+          }),
+        });
+        const res = await data.json();
+        alert_msg = res.msg;
+        if (res.success) {
+          alert_class = "alert-success";
+        } else {
+          alert_class = "alert-danger";
+        }
+      } catch (err) {
+        alert_msg = "An error occurred :(";
+        alert_class = "alert-danger";
+      }
+    }
+  }
 </script>
 
 <style>
-  .bg-teal {
-    background-color: #009688;
-  }
-  .roundcorner {
-    border-radius: 12px;
-  }
   #map {
-    width: 100%;
+    width: 480px;
     height: 360px;
   }
 </style>
 
 <main>
+  <link rel="stylesheet" href="/leaflet/leaflet.css" />
   <div class="container">
     <div class="row">
       <div class="col-lg-6 p-3">
@@ -23,18 +88,29 @@
         <form>
           <div class="form-group">
             <label class="control-label text-white" for="name">Check-point Name:</label>
-            <input type="text" class="form-control" id="name" name="name" />
+            <input
+              bind:value={name}
+              type="text"
+              class="form-control"
+              id="name"
+              name="name" />
           </div>
           <div class="form-group">
             <label class="control-label text-white" for="id">Device Id:</label>
-            <input type="text" class="form-control" id="id" name="id" />
+            <input
+              bind:value={id}
+              type="text"
+              class="form-control"
+              id="id"
+              name="id" />
           </div>
           <div class="form-group">
             <label
               class="control-label text-white"
               for="password">Password:</label>
             <input
-              type="text"
+              bind:value={pwd}
+              type="password"
               class="form-control"
               id="password"
               name="password" />
@@ -42,14 +118,14 @@
         </form>
       </div>
       <div class="col-lg-6 p-3">
-        <div class="form-group">
-          <label
-            class="control-label text-white"
-            for="location">Location:</label>
-          <div id="map" style="background-color:#ffffff;" />
-        </div>
+        <center>
+          <div id="map" />
+        </center>
       </div>
     </div>
-    <center><button class="btn bg-white">Submit</button></center>
+    <center>
+      <button on:click={submit} class="btn bg-white">Submit</button>
+    </center>
+    <div class="alert {alert_class} mt-2 mb-2" role="alert">{alert_msg}</div>
   </div>
 </main>
