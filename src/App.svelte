cd67ac8fe7cc0215045e78295ede157f4bd59511
Praveen Kumar S <praveens.bec@gmail.com>
2020-09-23 12:53:48 +0530

    Added leaflet.js

--- src/App.svelte
+++ src/App.svelte
@@ -3,10 +3,11 @@
 
   import Login from "./Login.svelte"
   import Register from "./Register.svelte"
+  import MapPage from "./MapPage.svelte"
 
   const SERVER = "http://127.0.0.1:3000";
   let current = Login;
-
+  document.body.style.backgroundColor = "#80cbc4";
   page('/', () => {
     if(localStorage.getItem("token"))
       current = Register;
@@ -24,6 +25,9 @@
   page('/logout',()=>{
     localStorage.setItem("token","");
     current = Login;
+  });
+  page('/map',()=>{
+    current = MapPage;
   })
   page.start();
 </script>
