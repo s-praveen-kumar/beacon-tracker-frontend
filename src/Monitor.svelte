e6f5e26924734a6524bee333f3ffa2cb5adc4066
Praveen Kumar S <praveens.bec@gmail.com>
2020-09-26 18:11:57 +0530

    Added vehicles and OSRM routes to Monitor page

--- src/Monitor.svelte
+++ src/Monitor.svelte
@@ -1,18 +1,31 @@
 <script>
   export let SERVER;
+  const OSRM_SERVER = "https://routing.openstreetmap.de/routed-car";
   import L from "leaflet";
   import { onMount } from "svelte";
-  import { each } from "svelte/internal";
+  import "./VehicleLi.svelte";
+  import VehicleLi from "./VehicleLi.svelte";
+  const getOptions = {
+    method: "GET",
+    mode: "cors",
+    credentials: "include",
+    headers: {
+      Authorization: "Bearer " + localStorage.getItem("token"),
+    },
+  };
+
   let map,
     checkpoints,
     markers = [];
+  let vehicles = [];
+  const vehiclePromise = fetchVehicles();
+
   onMount(() => {
     let options = {
       center: [0, 0],
       zoom: 2,
     };
     map = L.map("monitorMap", options);
-
     map.addLayer(
       L.tileLayer("https://tiles.wmflabs.org/hikebike/{z}/{x}/{y}.png", {
         attribution:
@@ -41,6 +54,7 @@
       } else {
         console.log(receivedCp.msg);
       }
+      fetchOSRM();
     } catch (err) {
       console.log(err);
       //Display Errors
@@ -61,16 +75,81 @@
     const markerGroup = L.featureGroup(markers);
     map.fitBounds(markerGroup.getBounds().pad(0.05));
   }
+
+  async function fetchVehicles() {
+    const data = await fetch(SERVER + "/active", getOptions);
+    const res = await data.json();
+    if (!res.success) throw Error("Error getting vehicles");
+    vehicles = res.vehicles;
+    return vehicles;
+  }
+
+  async function fetchOSRM() {
+    const data = await fetch(
+      OSRM_SERVER +
+        `/route/v1/car/${checkpoints[0].location.lon},${checkpoints[0].location.lat};${checkpoints[2].location.lon},${checkpoints[2].location.lat}?steps=false&geometries=geojson&overview=full`
+    );
+    const res = await data.json();
+    L.geoJSON(res.routes[0].geometry).addTo(map);
+    console.log(res.routes[0]);
+  }
 </script>
 
 <style>
+  .bg-teal {
+    background-color: #009688;
+  }
   #monitorMap {
     width: 100vw;
     height: 100vh;
+  }
+  .top-right {
+    position: fixed;
+    z-index: 1000;
+    top: 0;
+    right: 0;
+  }
+  #collapseVehicles {
+    position: fixed;
+    top: 0;
+    z-index: 1000;
+    right: 0;
   }
 </style>
 
 <main>
   <link rel="stylesheet" href="/leaflet/leaflet.css" />
   <div id="monitorMap" />
+
+  <button
+    class="btn bg-teal top-right text-white"
+    type="button"
+    data-toggle="collapse"
+    data-target="#collapseVehicles">
+    &#x1F441; &#x25BC;
+  </button>
+  <div class="collapse" id="collapseVehicles">
+    {#await vehiclePromise then vehicles}
+      {#each vehicles as vehicle}
+        <VehicleLi
+          vehicleNo={vehicle.vehicleNo}
+          driverName={vehicle.name}
+          beaconID={vehicle.beaconId} />
+      {/each}
+    {:catch err}
+      <div class="alert alert-danger" role="alert">
+        <h4 class="alert-heading">Oops!</h4>
+        <p>An error has occured :(</p>
+        <hr />
+        <a href="/logout" class="mb-0 alert-link">Go back to Login page</a>
+      </div>
+    {/await}
+    <button
+      class="btn bg-teal text-white"
+      type="button"
+      data-toggle="collapse"
+      data-target="#collapseVehicles">
+      &#x25B2;
+    </button>
+  </div>
 </main>
---
+++ src/VehicleLi.svelte
@@ -0,0 +1,12 @@
+<script>
+  export let vehicleNo;
+  export let driverName;
+  export let beaconID;
+</script>
+
+<div class="card p-2">
+  <div class="card-body p-2">
+    <h5 class="card-title"><strong>{beaconID}</strong> - {vehicleNo}</h5>
+    <h6 class="card-subtitle text-muted">{driverName}</h6>
+  </div>
+</div>
